406d7d9f0d396249db8943071ff176b42f6a58a7
Aditya Mhatre <aditya.r.mhatre@gmail.com>
2021-10-05 17:05:32 -0500

    push all

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const callsRouter = require('./routes/calls');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/call', callsRouter);
+
+app.listen(3000, '0.0.0.0');
+
+module.exports = app;
---
+++ routes/calls.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
+sendNotification = function (fcmToken, roomId, username) {
+    const message = {
+        data: { roomId, username },
+        token: fcmToken
+    };
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    return admin.messaging().send(message)
+        .then((response) => {
+            console.log('Successfully sent message:', response);
+            return response;
+        })
+        .catch((error) => {
+            console.log('Error sending message:', error);
+        });
+}
+router.post('/', async function (req, res, next) {
+    const response = await sendNotification(req.body['fcmToken'], req.body['roomId'], req.body['caller'])
+    res.send(response);
+});
+
+module.exports = router;
