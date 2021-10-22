15e996cdca42deee350460fa698aff8c286ee7fd
Aditya Mhatre <aditya.r.mhatre@gmail.com>
2021-10-21 20:27:52 -0500

    version lol

--- routes/version.js
+++ routes/version.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const version = 1.10
+const version = '1.10'
 
 router.get('/', async function (req, res, next) {
     res.send({ version });
